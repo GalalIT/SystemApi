2cf75de33ccd2aae8d911a140f2ad885786765a3
Galal <alslahyjlal424@gmail.com>
2025-03-28 01:38:25 -0800

    I build IDepartmentOparation in Interface that exist in Application.System

---
+++ Application.System/Interface/IDepartmentOparation/IAllDepartmentOparation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.System.Interface.IDepartmentOparation
+{
+    public interface IAllDepartmentOparation:IAddDepartmentOparation,IDeleteDepartmentOparation,IEditeDepartmentOparation,IGetAllDepartmentOparation,IGetByIdDepartmentOparation
+    {
+    }
+} 
