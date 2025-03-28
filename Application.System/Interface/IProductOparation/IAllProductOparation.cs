90f150d11567572b8497962cb810488b1501dffc
Galal <alslahyjlal424@gmail.com>
2025-03-28 02:22:17 -0800

    I build IProductOparation Interface that exist in Application.System

---
+++ Application.System/Interface/IProductOparation/IAllProductOparation.cs
@@ -0,0 +1,15 @@
+﻿using Application.System.Interface.IDepartmentOparation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.System.Interface.IProductOparation
+{
+    public interface IAllProductOparation: IAddProductOparation, IDeleteProductOparation, IEditeProductOparation, IGetAllProductOparation, IGetByIdProductOparation
+    {
+        Task<bool> HasRelatedRecords(int productId);
+    }
+}
+ 
---
+++ Application.System/Interface/IProductOparation/IGetAllProductOparation.cs
@@ -0,0 +1,21 @@
+﻿using Application.System.DTO;
+using Application.System.Interface.IBaseInterface;
+using Application.System.Utility;
+using Domin.System.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.System.Interface.IProductOparation
+{
+    public interface IGetAllProductOparation : IBaseGetAllAsync<ProductDTO>
+    {
+        Task<Response<List<Product>>> GetAllIncludeToUnitAsync();
+        Task<Response<List<Product>>> GetAllIncludeToDepartmentAsync();
+        Task<Response<List<Product>>> GetAllProductsByUserBranchAsync(int userBranchId);
+
+    }
+}
+ 
