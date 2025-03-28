57a6f5778c007ed0d18a0ae272c67af912360b30
Galal <alslahyjlal424@gmail.com>
2025-03-28 02:05:35 -0800

    I build IOrderOparation Interface that exist in Application.System

---
+++ Application.System/Interface/IOrderOparation/IAllOrderOparation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.System.Interface.IOrderOparation
+{
+    public interface IAllOrderOparation: IAddOrderOparation, IDeleteOrderOparation, IEditeOrderOparation, IGetAllOrderOparation, IGetByIdOrderOparation
+    {
+    }
+}
+ 
