4d394569369dbe79914c08f2eb5980c9e5758bd8
Galal <alslahyjlal424@gmail.com>
2025-03-28 02:13:11 -0800

    I build IProduct_UnitOpreation Interface that exist in Application.System

---
+++ Application.System/Interface/IProduct_UnitOpreation/IAllProduct_UnitOparation.cs
@@ -0,0 +1,18 @@
+﻿using Application.System.DTO;
+using Application.System.Utility;
+using Domin.System.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.System.Interface.IProduct_UnitOpreation
+{
+    public interface IAllProduct_UnitOparation : IAddProduct_UnitOparation, IDeleteProduct_UnitOparation, IEditeProduct_UnitOparation, IGetAllProduct_UnitOparation, IGetByIdProduct_UnitOparation
+    {
+        Task<Response<List<ProductUnitDTO>>> GetProductUnitsByProductIdAsync(int productId);
+
+    }
+} 
+ 
---
+++ Application.System/Interface/IProduct_UnitOpreation/IGetByIdProduct_UnitOparation.cs
@@ -0,0 +1,16 @@
+﻿using Application.System.DTO;
+using Application.System.Interface.IBaseInterface;
+using Domin.System.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.System.Interface.IProduct_UnitOpreation
+{
+    public interface IGetByIdProduct_UnitOparation : IBaseGetByIdAsync<ProductUnitDTO>
+    {
+    }
+}
+ 
