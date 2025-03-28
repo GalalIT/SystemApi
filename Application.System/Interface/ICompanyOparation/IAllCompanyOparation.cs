4843de9d8f0cd8990afabd2e0c9ff421bc8ef178
Galal <alslahyjlal424@gmail.com>
2025-03-28 01:25:38 -0800

    I build ICompanyOparation in Interface that exist in Application.System

---
+++ Application.System/Interface/ICompanyOparation/IAllCompanyOparation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.System.Interface.ICompanyOparation
+{
+    public interface IAllCompanyOparation: IAddCompanyOparation, IDeleteCompanyOparation, IEditeCompanyOparation, IGetAllCompanyOparation, IGetByIdCompanyOparation
+    { 
+    }
+}
