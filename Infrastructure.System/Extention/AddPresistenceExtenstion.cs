e7ca906268fde3d5c6122554c9dae89eb3ec9ddf
Galal <alslahyjlal424@gmail.com>
2025-04-01 23:08:49 -0800

    Ibuild AddPresistenceExtenstion that is exist in Infrastructure.System

--- Infrastructure.System/Extention/AddPresistenceExtenstion.cs
+++ Infrastructure.System/Extention/AddPresistenceExtenstion.cs
@@ -1,4 +1,32 @@
 ﻿
+using Application.System.Interface.IBranchOperation;
+using Application.System.Interface.ICompanyOperation;
+using Application.System.Interface.IDepartmentOperation;
+using Application.System.Interface.IOrderDetailsOperation;
+using Application.System.Interface.IOrderOperation;
+using Application.System.Interface.IProduct_UnitOperation;
+using Application.System.Interface.IProductOperation;
+using Application.System.Interface.IUnitOperation;
+using Application.System.Services.BranchServices;
+using Application.System.Services.CompanyServices;
+using Application.System.Services.DepartmentServices;
+using Application.System.Services.OrderDetailsServices;
+using Application.System.Services.OrderServices;
+using Application.System.Services.Product_UnitServices;
+using Application.System.Services.ProductServices;
+using Application.System.Services.UnitServices;
+using Application.System.UseCace.BranchUseCase.Implement;
+using Application.System.UseCace.BranchUseCase.Interface;
+using Application.System.UseCace.CompanyUseCase.Implement;
+using Application.System.UseCace.CompanyUseCase.Interface;
+using Application.System.UseCace.DepartmentUseCase.Implement;
+using Application.System.UseCace.DepartmentUseCase.Interface;
+using Application.System.UseCace.OrderUseCase.Implement;
+using Application.System.UseCace.OrderUseCase.Interface;
+using Application.System.UseCace.ProductUseCase.Implement;
+using Application.System.UseCace.ProductUseCase.Interface;
+using Application.System.UseCace.UnitUseCase.Implement;
+using Application.System.UseCace.UnitUseCase.Interface;
 using Domin.System.IRepository.IProduct_UnitRepository;
 using Domin.System.IRepository.IUnitOfRepository;
 using Infrastructure.System.Data;
@@ -20,34 +48,31 @@ namespace Infrastructure.System.Extention
             //////////////////////////////////////////Difine the UseCase ////////////////////
 
 
-            //services.AddScoped<AllBranchUseCase>();
-            //services.AddScoped<AllCompanyUseCase>();
-            //services.AddScoped<AllOrderDetailsUseCase>();
-            //services.AddScoped<AllProductUseCase>();
-            //services.AddScoped<AllUnitUseCase>();
-            //services.AddScoped<AllDepartmentUseCase>();
-            //services.AddScoped<AllOrderUseCase>();
-            //services.AddScoped<AllProduct_UnitUseCase>();
+            services.AddScoped<IBranchUseCase, BranchUseCase>();
+            services.AddScoped<ICompanyUseCase, CompanyUseCase>();
+            services.AddScoped<IProductUseCase, ProductUseCase>();
+            services.AddScoped<IUnitUseCase, UnitUseCase>();
+            services.AddScoped<IDepartmentUseCase, DepartmentUseCase>();
+            services.AddScoped<IOrderUseCase, OrderUseCase>();
 
             //////////////////////////////////////////Difine the Services ////////////////////
-            
-            //services.AddScoped<IAllBranchOparation, AllBranchServices>();
-            //services.AddScoped<IAllCompanyOparation, AllCompanyServices>();
-            //services.AddScoped<IAllDepartmentOparation, AllDepartmentServices>();
-            //services.AddScoped<IAllOrderDetailsOparation, AllOrderDetailsServices>();
-            //services.AddScoped<IAllOrderOparation, AllOrderServices>();
-            //services.AddScoped<IAllProductOparation, AllProductServices>();
-            //services.AddScoped<IAllUnitOparation, AllUnitServices>();
-            //services.AddScoped<IAllProduct_UnitOparation, AllProduct_UnitServices>();
+
+            services.AddScoped<IAllBranchOperation, AllBranchServices>();
+            services.AddScoped<IAllCompanyOperation, AllCompanyServices>();
+            services.AddScoped<IAllDepartmentOperation, AllDepartmentServices>();
+            services.AddScoped<IAllOrderDetailsOperation, AllOrderDetailsServices>();
+            services.AddScoped<IAllOrderOperation, AllOrderServices>();
+            services.AddScoped<IAllProductOperation, AllProductServices>();
+            services.AddScoped<IAllUnitOperation, AllUnitServices>();
+            services.AddScoped<IAllProduct_UnitOperation, AllProduct_UnitServices>();
 
             //////////////////////////////////////////Difine the Repository ////////////////////
             ///
 
-            //services.AddScoped<IAllProduct_UnitRepository, AllProduct_UnitRepository>();
-            //services.AddScoped<IUnitOfRepository, UnitOfRepository>();
+            services.AddScoped<IUnitOfRepository, UnitOfRepository>();
 
             ///
-           /////////////////////////////////////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////////////////////////////////////
             return services;
         }
     }
